49403bbd4e93680e8acf1b9e4d5fa67601d06602
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-07-01 13:19:55 +0100

    Fix wrong structure of moves.h

--- include/game/moves.h
+++ include/game/moves.h
@@ -1,27 +1,12 @@
-#include "utils.h"
 #include <cstdint>
 #include <vector>
 
 class Pawn {
 public:
-  std::vector<uint64_t> makeMoves(uint64_t state, bool colour) {
-    std::vector<uint64_t> moves;
-    uint64_t move;
-
-    if (colour) {
-      if (getRank(state) == 2) {
-        move = state << 8;
-      }
-    }
-
-    return moves;
-  }
+  std::vector<uint64_t> makeMoves(uint64_t state, bool colour);
 };
 
 class King {
 public:
-  std::vector<uint64_t> getPseudoLegalMoves() {
-    std::vector<uint64_t> moves;
-    return moves;
-  }
+  std::vector<uint64_t> getPseudoLegalMoves(uint64_t state, bool colour);
 };
---
+++ src/game/moves.cpp
@@ -0,0 +1,14 @@
+#include "game/moves.h"
+#include "utils.h"
+#include <cstdint>
+#include <vector>
+
+std::vector<uint64_t> Pawn::makeMoves(uint64_t state, bool colour) {
+  std::vector<uint64_t> moves;
+  return moves;
+};
+
+std::vector<uint64_t> King::getPseudoLegalMoves(uint64_t state, bool colour) {
+  std::vector<uint64_t> moves;
+  return moves;
+};
