560e3decb1bb0669098ab61283dfd7b4f4694355
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-07-15 18:28:36 +0100

    Docs

--- include/utils.h
+++ include/utils.h
@@ -6,27 +6,105 @@
 #include <regex>
 #include <vector>
 
+// For clarity
 using bitboard = uint64_t;
 using team = std::array<bitboard, 6>;
+
+// RegEx for recognising a valid input
 const std::regex squareRe{"[a-h][1-8]"};
 
+/*
+ * Returns the file (column) of a piece
+ *
+ * @param [state] bitboard with a single piece (single 1)
+ * @return integer corresponding to the file of the piece
+ */
 int getRank(bitboard state);
 
+/*
+ * Returns the rank (row) of a piece
+ *
+ * @param [state] bitboard with a single piece (single 1)
+ * @return integer corresponding to the rank of the piece
+ */
 int getFile(bitboard state);
 
+/*
+ * Given a chess coordinate (e.g. a3) in string form, returns a bitboard with a
+ * single piece at that coordinate
+ *
+ * @param [coord] coordinate in string form
+ * @return bitboard with a 1 at the coordinate only
+ */
 bitboard coordinateToState(const std::string &coord);
 
+/*
+ * Moves the provided piece north one square and returns it
+ *
+ * @param [state] bitboard with a single piece (single 1)
+ * @return bitboard with the piece moved up one square
+ */
 bitboard slideNorth(bitboard state);
-bitboard slideEast(bitboard state);
+/*
+ * Moves the provided piece south one square and returns it
+ *
+ * @param [state] bitboard with a single piece (single 1)
+ * @return bitboard with the piece moved down one square
+ */
 bitboard slideSouth(bitboard state);
+
+/*
+ * Moves the provided piece east one square and returns it
+ *
+ * @param [state] bitboard with a single piece (single 1)
+ * @return bitboard with the piece moved right one square
+ */
+bitboard slideEast(bitboard state);
+
+/*
+ * Moves the provided piece west one square and returns it
+ *
+ * @param [state] bitboard with a single piece (single 1)
+ * @return bitboard with the piece moved left one square
+ */
 bitboard slideWest(bitboard state);
 
+/*
+ * Returns an array of bitboards, each with a single piece, from a bitboard with
+ * multiple pieces
+ *
+ * @param [state] bitboard with multiple pieces, which each piece will be
+ * extracted from
+ * @return vector of bitboards, each with a single piece from the original board
+ * on it
+ */
 std::vector<bitboard> getAllPieces(bitboard state);
 
+/*
+ * Searches for a piece in an array which contains a 1 at the location specified
+ * in the bitboard of square
+ *
+ * @param [square] bitboard containing a single piece which is to be searched
+ * for
+ * @param [target] array of bitboards which are to be searched
+ * @return pair containing <bitboard which contains the square, index of the
+ * bitboard in the searched array>
+ */
 std::pair<bitboard, int> findPiece(bitboard square,
                                    std::array<bitboard, 6> &target);
 
+/*
+ * Capture logic for a board containing possibly many pieces (victim), and a
+ * single piece (captor) which will capture one of those pieces. Returns victim
+ * after capture
+ *
+ * @param [victim] bitboard to be captured
+ * @param [captor] bitboard with a single piece, will capture one of victim's
+ * pieces
+ * @return victim after capture
+ */
 bitboard performCapture(bitboard victim, bitboard captor);
 
+// Clears the terminal window, intended to preceed printing of the board
 void clearTerm();
 #endif // !_UTILSH_
--- src/utils.cpp
+++ src/utils.cpp
@@ -6,12 +6,19 @@
 #include <utility>
 #include <vector>
 
-/*
- * Returns the file (column) of a piece
- *
- * @param [state] bitboard with a single piece (single 1)
- * @return integer corresponding to the file of the piece
- */
+int getRank(uint64_t state) {
+    // Asserting there are not more than one piece on the board
+    assert(__builtin_popcountll(state) == 1);
+    // Shift downwards until we reach 0
+    int rank = 0;
+    uint64_t tempState = state;
+    while (tempState != 0) {
+        tempState >>= 8;
+        rank++;
+    }
+    return rank;
+}
+
 int getFile(uint64_t state) {
     // Asserting there are not more than one piece on the board
     assert(__builtin_popcountll(state) == 1);
@@ -31,96 +38,6 @@ int getFile(uint64_t state) {
     return 8 - exp;
 }
 
-/*
- * Returns the rank (row) of a piece
- *
- * @param [state] bitboard with a single piece (single 1)
- * @return integer corresponding to the rank of the piece
- */
-int getRank(uint64_t state) {
-    // Asserting there are not more than one piece on the board
-    assert(__builtin_popcountll(state) == 1);
-    // Shift downwards until we reach 0
-    int rank = 0;
-    uint64_t tempState = state;
-    while (tempState != 0) {
-        tempState >>= 8;
-        rank++;
-    }
-    return rank;
-}
-
-/*
- * Moves the provided piece north one square and returns it
- *
- * @param [state] bitboard with a single piece (single 1)
- * @return bitboard with the piece moved up one square
- */
-bitboard slideNorth(bitboard state) {
-    assert(__builtin_popcountll(state) == 1);
-    return state <<= 8;
-}
-
-/*
- * Moves the provided piece south one square and returns it
- *
- * @param [state] bitboard with a single piece (single 1)
- * @return bitboard with the piece moved down one square
- */
-bitboard slideSouth(bitboard state) {
-    assert(__builtin_popcountll(state) == 1);
-    return state >>= 8;
-}
-
-/*
- * Moves the provided piece east one square and returns it
- *
- * @param [state] bitboard with a single piece (single 1)
- * @return bitboard with the piece moved right one square
- */
-bitboard slideEast(bitboard state) {
-    assert(__builtin_popcountll(state) == 1);
-    return (state >>= 1) & 0x7F7F7F7F7F7F7F7FULL;
-}
-
-/*
- * Moves the provided piece west one square and returns it
- *
- * @param [state] bitboard with a single piece (single 1)
- * @return bitboard with the piece moved left one square
- */
-uint64_t slideWest(uint64_t state) {
-    assert(__builtin_popcountll(state) == 1);
-    return (state <<= 1) & 0xFEFEFEFEFEFEFEFE;
-}
-
-/*
- * Returns an array of bitboards, each with a single piece, from a bitboard with
- * multiple pieces
- *
- * @param [state] bitboard with multiple pieces, which each piece will be
- * extracted from
- * @return vector of bitboards, each with a single piece from the original board
- * on it
- */
-std::vector<bitboard> getAllPieces(bitboard state) {
-    bitboard initState = state;
-    std::vector<bitboard> pieces;
-    while (initState != 0) {
-        bitboard thisPiece = initState & -initState;
-        pieces.push_back(thisPiece);
-        initState &= initState - 1;
-    }
-    return pieces;
-}
-
-/*
- * Given a chess coordinate (e.g. a3) in string form, returns a bitboard with a
- * single piece at that coordinate
- *
- * @param [coord] coordinate in string form
- * @return bitboard with a 1 at the coordinate only
- */
 bitboard coordinateToState(const std::string &coord) {
     // Get rank and file to determine how many times we must shift
     int file = coord.at(0) - 'a';
@@ -140,16 +57,29 @@ bitboard coordinateToState(const std::string &coord) {
     return state;
 }
 
-/*
- * Searches for a piece in an array which contains a 1 at the location specified
- * in the bitboard of square
- *
- * @param [square] bitboard containing a single piece which is to be searched
- * for
- * @param [target] array of bitboards which are to be searched
- * @return pair containing <bitboard which contains the square, index of the
- * bitboard in the searched array>
- */
+bitboard slideNorth(bitboard state) { return state <<= 8; }
+
+bitboard slideSouth(bitboard state) { return state >>= 8; }
+
+bitboard slideEast(bitboard state) {
+    return (state >>= 1) & 0x7F7F7F7F7F7F7F7FULL;
+}
+
+uint64_t slideWest(uint64_t state) {
+    return (state <<= 1) & 0xFEFEFEFEFEFEFEFE;
+}
+
+std::vector<bitboard> getAllPieces(bitboard state) {
+    bitboard initState = state;
+    std::vector<bitboard> pieces;
+    while (initState != 0) {
+        bitboard thisPiece = initState & -initState;
+        pieces.push_back(thisPiece);
+        initState &= initState - 1;
+    }
+    return pieces;
+}
+
 std::pair<bitboard, int> findPiece(bitboard square, team &target) {
     // Ensure we are only searching for a single location
     assert(__builtin_popcountll(square) == 1);
@@ -163,16 +93,6 @@ std::pair<bitboard, int> findPiece(bitboard square, team &target) {
     return std::make_pair(0, -1);
 }
 
-/*
- * Capture logic for a board containing possibly many pieces (victim), and a
- * single piece (captor) which will capture one of those pieces. Returns victim
- * after capture
- *
- * @param [victim] bitboard to be captured
- * @param [captor] bitboard with a single piece, will capture one of victim's
- * pieces
- * @return victim after capture
- */
 bitboard performCapture(bitboard victim, bitboard captor) {
     assert((victim & captor) != 0);
     assert(__builtin_popcountll(captor) == 1);
