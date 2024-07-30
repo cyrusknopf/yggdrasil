15f86f359553bdb40dd0c72a6741d912dd5b1e57
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-07-30 11:51:48 +0100

    Docs backprop.h

--- include/mcts/backprop.h
+++ include/mcts/backprop.h
@@ -1,3 +1,10 @@
 #include "mcts/gametree.h"
 
+/*
+ * Given a terminal node in the game tree, updates the values of nodes above
+ * based on the winner, i.e. nodes whose turn = winner have their wins
+ * incremented
+ * @param [node] terminal game node
+ * @param [winner] winner at this node
+ */
 void backpropagate(GameNode* node, bool winner);
