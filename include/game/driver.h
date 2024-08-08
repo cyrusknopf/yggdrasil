e01a76dc7c139d6a94f967eb955c50b0465c93e1
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-08 22:02:17 +0100

    driver.h docs

--- include/game/driver.h
+++ include/game/driver.h
@@ -4,12 +4,16 @@
 /*
  * Contains logic for running a game where the user is white and MCTS agent is
  * black
+ *
+ * @param [moveTime] time given to the MCTS agent to make a move
  */
 void userVsMCTS(int moveTime);
 
 /*
  * Contains logic for running a game where the random agent is white and MCTS
  * agent is black
+ *
+ * @param [moveTime] time given to the MCTS agent to make a move
  */
 void randomVsMCTS(int moveTime);
 
