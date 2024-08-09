97d20e71844c7fad93e51b21fae89bb6780bbb87
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-09 10:25:47 +0100

    backprop.h,.cpp docs

--- include/mcts/backprop.h
+++ include/mcts/backprop.h
@@ -1,10 +1,13 @@
 #include "mcts/gametree.h"
 
 /*
- * Given a terminal node in the game tree, updates the values of nodes above
- * based on the winner, i.e. nodes whose turn = winner have their wins
+ * Given a node in the game tree, which has had a random playout done from it,
+ * updates the path from that node to the root based on the outcome of the game.
+ * All ancestors have their `vists` member incremented, while only the nodes who
+ * represent a move made by the winner of the playout have their wins
  * incremented
- * @param [node] terminal game node
- * @param [winner] winner at this node
+ * @param [node] game node from which the playout occurs
+ * @param [winner] winner at this node: true = white, false = black, nullopt =
+ * stalemate
  */
 void backpropagate(GameNode* node, std::optional<bool> winner);
--- src/mcts/backprop.cpp
+++ src/mcts/backprop.cpp
@@ -1,9 +1,13 @@
 #include "mcts/backprop.h"
 
 void backpropagate(GameNode* node, std::optional<bool> winner) {
+    // Increment visits regardless of outcome
     node->incrVisits();
+    // If not a stalemate, we need to update `wins` member
     if (winner.has_value())
+        // XXX should be reversed
         if (node->getTurn() == winner) node->incrWins();
+    // If this node is not the root, we must update ancestors
     if (node->getParent() != nullptr)
         return backpropagate(node->getParent(), winner);
     return;
