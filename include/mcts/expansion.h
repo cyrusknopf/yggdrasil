119042fcc7d5af7aef620731ff566504d122a666
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-09 10:30:50 +0100

    expansion.h,.cpp docs

--- include/mcts/expansion.h
+++ include/mcts/expansion.h
@@ -1,4 +1,10 @@
 
 #include "mcts/gametree.h"
 
+/*
+ * Adds children to the node passed as `parent`: one child for each legal move
+ * at the game state of `parent`.
+ *
+ * @param [parent] node to add children to
+ */
 void expansion(GameNode* parent);
--- src/mcts/expansion.cpp
+++ src/mcts/expansion.cpp
@@ -12,9 +12,11 @@
 #include "utils.h"
 
 void expansion(GameNode* parent) {
+    // Get the games state
     team white = parent->getWhite();
     team black = parent->getBlack();
 
+    // `parent`'s turn is the one to move next: get moves for that player
     std::vector<std::pair<bitboard, int>> moves =
         getAllLegalMoves(white, black, parent->getTurn());
 
@@ -27,17 +29,22 @@ void expansion(GameNode* parent) {
         GameNode* child = parent->addChild(parent, move, newWhite, newBlack);
         team childWhite = child->getWhite();
         team childBlack = child->getBlack();
-        // TODO results in backprop the wrong value in driver
+        // TODO? results in backprop the wrong value in driver
+        // Given the new gamestate, if there are now no legal moves to be made,
+        // the game is a stalemate
         if (getAllLegalMoves(childWhite, childBlack, child->getTurn())
                 .empty()) {
             child->setTerminal();
             // Do not set winner, keep as nullopt to denote draw
         };
-        // Black wins
+        // Given the new gamestate, if either player is mated, set the node to
+        // be terminal and set the winner of the node
+        // TODO only need to check one here: i.e. white can never be mated if
+        // white just moved
         if (isMated(newWhite, newBlack, true)) {
             child->setTerminal();
             child->setWinner(false);
-            // white wins
+            // White wins
         } else if (isMated(newWhite, newBlack, false)) {
             child->setTerminal();
             child->setWinner(true);
