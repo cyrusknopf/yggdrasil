85b132544a4e068d48dcf1a7ca3e9cf16c17dcb4
Cyrus Knopf <cyrus.knopf@gmail.com>
2024-08-13 12:58:59 +0100

    Added pawn promotion for MCTS
    Not implemented for random or user

--- tests/mcts/expansion_tests.cpp
+++ tests/mcts/expansion_tests.cpp
@@ -69,3 +69,33 @@ TEST(expansion, mateIn1) {
         ASSERT_FALSE(children.at(i)->getTerminal());
     }
 }
+
+TEST(expansion, promotion) {
+    bitboard whitePawn = coordinateToState("a7");
+    bitboard whiteKing = coordinateToState("h1");
+    bitboard blackKing = coordinateToState("b1");
+
+    team white = {whitePawn, 0, 0, 0, 0, whiteKing};
+    team black = {0, 0, 0, 0, 0, blackKing};
+
+    GameNode* node = new GameNode(nullptr, 0, white, black, true);
+
+    ASSERT_EQ(0, node->getChildren().size());
+
+    expansion(node);
+
+    std::vector<GameNode*> children = node->getChildren();
+    // 1 pawn push with four possible promotions. 3 moves for king
+    ASSERT_EQ((1 * 4) + 3, children.size());
+
+    for (int i = 0; i < 4; i++) {
+        GameNode* promote = children.at(i);
+        // Assert the move is the result of pawn push
+        ASSERT_EQ(promote->getMove(), coordinateToState("a8"));
+        // Assert the pawn has been promoted i.e. removed
+        ASSERT_EQ(promote->getWhite().at(0), 0);
+        // Assert the piece has taken the pawn's place
+        // (piece index = i + 1)i.e 1st promotion(index 0)=horse(piece index 1))
+        ASSERT_EQ(promote->getWhite().at(i + 1), coordinateToState("a8"));
+    }
+}
